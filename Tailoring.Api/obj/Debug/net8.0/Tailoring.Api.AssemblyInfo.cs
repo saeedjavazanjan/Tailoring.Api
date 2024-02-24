6bb908716d938fb6aff089b15dc5475e9f5cff7c
saeed <ghfarisaeed1@gmail.com>
2024-02-24 11:57:07 +0330

    commit dll files

--- Tailoring.Api/obj/Debug/net8.0/Tailoring.Api.AssemblyInfo.cs
+++ Tailoring.Api/obj/Debug/net8.0/Tailoring.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tailoring.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+589d066b01d6c7ae01dc12d9012f5de151b70947")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tailoring.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tailoring.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
